66f51e034ad67da80b2149b28d9753631e4de9de
rachelw106 <rachelw106@nycstudents.net>
2024-10-16 13:28:53 +0000

    commit

--- WordChecker.java
+++ WordChecker.java
@@ -1,4 +1,45 @@
-public class WordChecker 
-{
+import java.util.ArrayList;
 
+public class WordChecker {
+    /** Initialized in the constructor and contains no null elements */
+    private ArrayList<String> wordList;
+
+    public WordChecker()
+    {
+        wordList = new ArrayList<String> ();
+    }
+    public WordChecker(ArrayList<String> list)
+    {
+        wordList = list;
+    }
+
+    /**
+     * Returns true if each element of wordList (except the first) contains the
+     * previous
+     * element as a substring and returns false otherwise, as described in part (a)
+     * Precondition: wordList contains at least two elements.
+     * Postcondition: wordList is unchanged.
+     */
+    public boolean isWordChain() {
+        boolean wordChain = true;
+        for(int i = 1; i < wordList.size(); i++)
+        {
+            String after = wordList.get(i);
+            String before = wordList.get(i-1);
+            if(after.indexOf(before)<0) return false;
+        }
+        return wordChain;
+    }
+
+    /**
+     * Returns an ArrayList<String> based on strings from wordList that start
+     * with target, as described in part (b). Each element of the returned ArrayList
+     * has had
+     * the initial occurrence of target removed.
+     * Postconditions: wordList is unchanged.
+     * Items appear in the returned list in the same order as they appear in
+     * wordList.
+     */
+    public ArrayList<String> createList(String target) {
+        /* to be implemented in part (b) */ }
 }
